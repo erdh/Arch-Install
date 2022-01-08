f5f1c08a83e0a3443920c2d57fd56f29ce7e57af
erdh <47626170+erdh@users.noreply.github.com>
2022-01-08 19:49:55 +0300

    Added license to 01-Adana.sh

--- 01-Adana.sh
+++ 01-Adana.sh
@@ -1,3 +1,20 @@
+: '
+01-Adana.sh, pre-install script for Arch Linux installer.
+    Copyright (C) 2021  Erdem Harputlugil
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <https://www.gnu.org/licenses/>. 
+'
 #!/usr/bin/env bash
 #This script is pre-install script for Arch install, Adana-01
 #Mostly this installer is going to use whiptail as ui for more user friendly use
